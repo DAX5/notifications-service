1e562380f6616b379c6528c05403313b75ac2cce
Douglas Silva <douglas.rafa.silva@gmail.com>
2022-12-20 15:31:00 -0300

    Initial commit

---
+++ src/infra/messaging/kafka/kafka-consumer.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, OnModuleDestroy } from "@nestjs/common";
+import { ServerKafka } from "@nestjs/microservices";
+
+@Injectable()
+export class KafkaConsumerService extends ServerKafka implements OnModuleDestroy {
+  constructor() {
+    super({
+      client: {
+        clientId: 'notifications',
+        brokers: ['happy-alpaca-10118-us1-kafka.upstash.io:9092'],
+        sasl: {
+          mechanism: 'scram-sha-256',
+          username: '',
+          password: '',
+        },
+        ssl: true,
+      }
+    })
+  }
+
+  async onModuleDestroy() {
+    await this.close();
+  }
+}
